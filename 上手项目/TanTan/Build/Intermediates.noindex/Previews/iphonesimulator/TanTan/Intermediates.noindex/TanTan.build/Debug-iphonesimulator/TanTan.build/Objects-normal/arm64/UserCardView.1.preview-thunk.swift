d9f27acb004a1b6298e38daab6abb58868e44432
huipukui <huipukui@autohome.com.cn>
2024-10-18 00:24:03 +0800

    滑动手势

--- 上手项目/TanTan/Build/Intermediates.noindex/Previews/iphonesimulator/TanTan/Intermediates.noindex/TanTan.build/Debug-iphonesimulator/TanTan.build/Objects-normal/arm64/UserCardView.1.preview-thunk.swift
+++ 上手项目/TanTan/Build/Intermediates.noindex/Previews/iphonesimulator/TanTan/Intermediates.noindex/TanTan.build/Debug-iphonesimulator/TanTan.build/Objects-normal/arm64/UserCardView.1.preview-thunk.swift
@@ -10,9 +10,51 @@ import SwiftUI
 
 extension UserCardView {
     @_dynamicReplacement(for: updateimageIndex(hasMoreImage:)) private func __preview__updateimageIndex(hasMoreImage: Bool) {
-        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 51)
-        let nextIndex = hasMoreImage ? imageIndex + __designTimeInteger("#2741.[1].[3].[0].value.then.[0]", fallback: 1) : imageIndex - __designTimeInteger("#2741.[1].[3].[0].value.else.[0]", fallback: 1)
-        imageIndex = min(max(__designTimeInteger("#2741.[1].[3].[1].[0]", fallback: 0), nextIndex), userCard.photos.count - __designTimeInteger("#2741.[1].[3].[1].[1]", fallback: 1))
+        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 103)
+        let nextIndex = hasMoreImage ? imageIndex + __designTimeInteger("#2741.[1].[7].[0].value.then.[0]", fallback: 1) : imageIndex - __designTimeInteger("#2741.[1].[7].[0].value.else.[0]", fallback: 1)
+        imageIndex = min(max(__designTimeInteger("#2741.[1].[7].[1].[0]", fallback: 0), nextIndex), userCard.photos.count - __designTimeInteger("#2741.[1].[7].[1].[1]", fallback: 1))
+    
+#sourceLocation()
+    }
+}
+
+extension UserCardView {
+    @_dynamicReplacement(for: createUserCardLabel(title:degree:color:)) private func __preview__createUserCardLabel(title: String, degree: Double, color: Color) -> some View {
+        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 90)
+        Text(title)
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.horizontal)
+            .foregroundColor(color)
+            .overlay(
+                RoundedRectangle(cornerRadius: __designTimeInteger("#2741.[1].[6].[0].modifier[4].arg[0].value.arg[0].value", fallback: 5))
+                    .stroke(color, lineWidth: __designTimeInteger("#2741.[1].[6].[0].modifier[4].arg[0].value.modifier[0].arg[1].value", fallback: 3))
+            )
+            .rotationEffect(Angle(degrees: degree))
+    
+#sourceLocation()
+    }
+}
+
+extension UserCardView {
+    @_dynamicReplacement(for: getRotationAmount()) private func __preview__getRotationAmount() -> Double {
+        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 83)
+        let max = UIScreen.main.bounds.width / __designTimeInteger("#2741.[1].[5].[0].value.[0]", fallback: 2)
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        return Double(percentage * __designTimeInteger("#2741.[1].[5].[3].arg[0].value.[0]", fallback: 10))
+    
+#sourceLocation()
+    }
+}
+
+extension UserCardView {
+    @_dynamicReplacement(for: getScaleAmount()) private func __preview__getScaleAmount() -> CGFloat {
+        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 76)
+        let max = UIScreen.main.bounds.width / __designTimeInteger("#2741.[1].[4].[0].value.[0]", fallback: 2)
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return __designTimeFloat("#2741.[1].[4].[3].[0]", fallback: 1.0) - min(percentage, __designTimeFloat("#2741.[1].[4].[3].[1]", fallback: 0.7)) * __designTimeFloat("#2741.[1].[4].[3].[2]", fallback: 0.3)
     
 #sourceLocation()
     }
@@ -20,7 +62,7 @@ extension UserCardView {
 
 extension UserCardView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
-        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 16)
+        #sourceLocation(file: "/Users/huipukui/Desktop/All/iOSLearn/swiftUILearn/上手项目/TanTan/TanTan/UserCardView.swift", line: 17)
         GeometryReader { proxy in
             let frameWidth = proxy.size.width
             let frameHeight = proxy.size.height
@@ -29,30 +71,54 @@ extension UserCardView {
                 .resizable()
                 .frame(width: frameWidth, height: frameHeight)
                 .aspectRatio(contentMode: .fit)
-                .cornerRadius(__designTimeInteger("#2741.[1].[2].property.[0].[0].arg[0].value.[2].modifier[3].arg[0].value", fallback: 20))
+                .cornerRadius(__designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[2].modifier[3].arg[0].value", fallback: 20))
             
             HStack {
                 Rectangle()
                     .onTapGesture {
-                        updateimageIndex(hasMoreImage: __designTimeBoolean("#2741.[1].[2].property.[0].[0].arg[0].value.[3].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[0].value", fallback: false))
+                        updateimageIndex(hasMoreImage: __designTimeBoolean("#2741.[1].[3].property.[0].[0].arg[0].value.[3].arg[0].value.[0].modifier[0].arg[0].value.[0].arg[0].value", fallback: false))
                     }
                 Rectangle()
                     .onTapGesture {
-                        updateimageIndex(hasMoreImage: __designTimeBoolean("#2741.[1].[2].property.[0].[0].arg[0].value.[3].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[0].value", fallback: true))
+                        updateimageIndex(hasMoreImage: __designTimeBoolean("#2741.[1].[3].property.[0].[0].arg[0].value.[3].arg[0].value.[1].modifier[0].arg[0].value.[0].arg[0].value", fallback: true))
                     }
             }
-            .foregroundColor(.white.opacity(__designTimeFloat("#2741.[1].[2].property.[0].[0].arg[0].value.[3].modifier[0].arg[0].value.modifier[0].arg[0].value", fallback: 0.01)))
+            .foregroundColor(.white.opacity(__designTimeFloat("#2741.[1].[3].property.[0].[0].arg[0].value.[3].modifier[0].arg[0].value.modifier[0].arg[0].value", fallback: 0.01)))
             
             HStack {
-                ForEach(__designTimeInteger("#2741.[1].[2].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[0].value.[0]", fallback: 0)..<userCard.photos.count, id: \.self) { imageIndex in
-                    RoundedRectangle(cornerRadius: __designTimeInteger("#2741.[1].[2].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].arg[0].value", fallback: 20))
-                        .frame(height: __designTimeInteger("#2741.[1].[2].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].modifier[0].arg[0].value", fallback: 4))
-                        .foregroundColor(self.imageIndex == imageIndex ? .white : .gray).opacity(__designTimeFloat("#2741.[1].[2].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].modifier[2].arg[0].value", fallback: 0.5))
+                ForEach(__designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[0].value.[0]", fallback: 0)..<userCard.photos.count, id: \.self) { imageIndex in
+                    RoundedRectangle(cornerRadius: __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].arg[0].value", fallback: 20))
+                        .frame(height: __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].modifier[0].arg[0].value", fallback: 4))
+                        .foregroundColor(self.imageIndex == imageIndex ? .white : .gray).opacity(__designTimeFloat("#2741.[1].[3].property.[0].[0].arg[0].value.[4].arg[0].value.[0].arg[2].value.[0].modifier[2].arg[0].value", fallback: 0.5))
                 }
             }
-            .padding(.top, __designTimeInteger("#2741.[1].[2].property.[0].[0].arg[0].value.[4].modifier[0].arg[1].value", fallback: 10))
+            .padding(.top, __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[4].modifier[0].arg[1].value", fallback: 10))
             .padding(.horizontal)
+            
+            HStack {
+                if offset.width > 0 {
+                    createUserCardLabel(title: __designTimeString("#2741.[1].[3].property.[0].[0].arg[0].value.[5].arg[0].value.[0].[0].[0].arg[0].value", fallback: "LIKE"), degree: __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[5].arg[0].value.[0].[0].[0].arg[1].value", fallback: -20), color: .green)
+                    Spacer()
+                } else if offset.width < 0 {
+                    Spacer()
+                    createUserCardLabel(title: __designTimeString("#2741.[1].[3].property.[0].[0].arg[0].value.[5].arg[0].value.[0].[1].[1].arg[0].value", fallback: "NOPE"), degree: __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[5].arg[0].value.[0].[1].[1].arg[1].value", fallback: 20), color: .red)
+                }
+            }
+            .padding(.horizontal, __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[5].modifier[0].arg[1].value", fallback: 30))
+            .padding(.top, __designTimeInteger("#2741.[1].[3].property.[0].[0].arg[0].value.[5].modifier[1].arg[1].value", fallback: 40))
         }
+        .offset(offset)
+        .scaleEffect(getScaleAmount())
+        .rotationEffect(Angle(degrees: getRotationAmount()))
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.offset = value.translation
+                }
+                .onEnded { value in
+                    self.offset = .zero
+                }
+        )
     
 #sourceLocation()
     }
--- 上手项目/TanTan/TanTan/UserCardView.swift
+++ 上手项目/TanTan/TanTan/UserCardView.swift
@@ -11,6 +11,7 @@ struct UserCardView: View {
     
     var userCard: UserCard
     @State var imageIndex = 0
+    @State var offset: CGSize = .zero
     
     var body: some View {
         GeometryReader { proxy in
@@ -44,7 +45,58 @@ struct UserCardView: View {
             }
             .padding(.top, 10)
             .padding(.horizontal)
+            
+            HStack {
+                if offset.width > 0 {
+                    createUserCardLabel(title: "LIKE", degree: -20, color: .green)
+                    Spacer()
+                } else if offset.width < 0 {
+                    Spacer()
+                    createUserCardLabel(title: "NOPE", degree: 20, color: .red)
+                }
+            }
+            .padding(.horizontal, 30)
+            .padding(.top, 40)
         }
+        .offset(offset)
+        .scaleEffect(getScaleAmount())
+        .rotationEffect(Angle(degrees: getRotationAmount()))
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.offset = value.translation
+                }
+                .onEnded { value in
+                    self.offset = .zero
+                }
+        )
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.7) * 0.3
+    }
+    
+    func getRotationAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = offset.width
+        let percentage = currentAmount / max
+        return Double(percentage * 10)
+    }
+    
+    func createUserCardLabel(title: String, degree: Double, color: Color) -> some View {
+        Text(title)
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.horizontal)
+            .foregroundColor(color)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(color, lineWidth: 3)
+            )
+            .rotationEffect(Angle(degrees: degree))
     }
     
     func updateimageIndex(hasMoreImage: Bool) {
